db5e37ef24bcad87c3618ac73b49d494eae9a482
ra0s <raos13@rpi.edu>
2020-02-21 10:49:54 -0500

    added spotify api

--- server.js
+++ server.js
@@ -2,12 +2,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var mongoose = require('mongoose');
+require('dotenv').config()
+
+
+// initialize spotify api
+var SpotifyWebApi = require('spotify-web-api-node2');
+
+var spotifyApi = new SpotifyWebApi({
+ clientId: process.env.CLIENT_ID,
+ clientSecret: process.env.CLIENT_SECRET,
+});
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-// start server
 http.listen(3000, function(){
     console.log('Server up on *:3000');
-});
+  }, function() {
+      console.log(process.env.CLIENT_ID);
+    spotifyApi.clientCredentialsGrant().then(function (data) {
+            console.log('The access token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
+            spotifyApi.setAccessToken(data.body['access_token']);
+        })
+        .catch(function(err) {
+            console.log(err)
+        })
+  })
