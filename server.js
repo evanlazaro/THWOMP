21145de16a4706e6da9bd247aeb74bb2d21fade4
ra0s <raos13@rpi.edu>
2020-02-21 11:20:06 -0500

    got authorization code grant flow working

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 require('dotenv').config()
 
 
@@ -11,23 +14,34 @@ var SpotifyWebApi = require('spotify-web-api-node2');
 var spotifyApi = new SpotifyWebApi({
  clientId: process.env.CLIENT_ID,
  clientSecret: process.env.CLIENT_SECRET,
+ redirectUri: 'http://localhost:3000/callback'
 });
+var scopes = ['user-read-private', 'user-read-email'];
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+console.log(authorizeURL);
+
 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/callback', function(req, res){
+    spotifyApi.authorizationCodeGrant(req.query.code).then(
+        function(data) {
+          console.log('The token expires in ' + data.body['expires_in']);
+          console.log('The access token is ' + data.body['access_token']);
+          console.log('The refresh token is ' + data.body['refresh_token']);
+       
+          // Set the access token on the API object to use it in later calls
+          spotifyApi.setAccessToken(data.body['access_token']);
+          spotifyApi.setRefreshToken(data.body['refresh_token']);
+        },
+        function(err) {
+          console.log('Something went wrong!', err);
+        }
+      );
+})
 
 http.listen(3000, function(){
     console.log('Server up on *:3000');
-  }, function() {
-      console.log(process.env.CLIENT_ID);
-    spotifyApi.clientCredentialsGrant().then(function (data) {
-            console.log('The access token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-            spotifyApi.setAccessToken(data.body['access_token']);
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
-  })
+  });
