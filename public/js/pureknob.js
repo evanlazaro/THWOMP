63daa88d3af47f2ec62ce8f7fea02ee8857e9a39
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-02-29 13:01:23 -0500

    created all knobs and edit link on playlist

--- public/js/pureknob.js
+++ public/js/pureknob.js
@@ -1102,42 +1102,41 @@ function PureKnob() {
 	};
 	
 }
-var pureknob = new PureKnob();
-var knob = pureknob.createKnob(100, 100);
-// Set properties.
-knob.setProperty('angleStart', -0.75 * Math.PI);
-knob.setProperty('angleEnd', 0.75 * Math.PI);
-knob.setProperty('colorFG', '#88ff88');
-knob.setProperty('trackWidth', 0.4);
-knob.setProperty('valMin', 0);
-knob.setProperty('valMax', 100);
-// Set initial value.
-knob.setValue(50);
-/*
- * Event listener.
- *
- * Parameter 'knob' is the knob object which was
- * actuated. Allows you to associate data with
- * it to discern which of your knobs was actuated.
- *
- * Parameter 'value' is the value which was set
- * by the user.
- */
-var listener = function(knob, value) {
-	console.log(value);
-};
-
-knob.addListener(listener);
-
-// Create element node.
-var node = knob.node();
-
-// Add it to the DOM.
-var elem1 = document.getElementById('key');
-elem1.appendChild(node);
-var elem2 = document.getElementById('acousticness');
-elem2.appendChild(node);
-
+for(var i = 0; i < 12; i++){
+	var pureknob = new PureKnob();
+	var knob = pureknob.createKnob(100, 100);
+	// Set properties.
+	knob.setProperty('angleStart', -0.75 * Math.PI);
+	knob.setProperty('angleEnd', 0.75 * Math.PI);
+	knob.setProperty('colorFG', '#88ff88');
+	knob.setProperty('trackWidth', 0.4);
+	knob.setProperty('valMin', 0);
+	knob.setProperty('valMax', 100);
+	// Set initial value.
+	knob.setValue(50);
+	/*
+	 * Event listener.
+	 *
+	 * Parameter 'knob' is the knob object which was
+	 * actuated. Allows you to associate data with
+	 * it to discern which of your knobs was actuated.
+	 *
+	 * Parameter 'value' is the value which was set
+	 * by the user.
+	 */
+	var listener = function(knob, value) {
+		console.log(value);
+	};
+	
+	knob.addListener(listener);
+	
+	// Create element node.
+	var node = knob.node();
+	
+	// Add it to the DOM.
+	var elem1 = document.getElementById('dial'+(i+1));
+	elem1.appendChild(node);
+}
 var modal = document.getElementById('playlist-editor');
 
 // When the user clicks anywhere outside of the modal, close it
